bb2f83d169337a736a2f7cdc6854b50b515f6016
Gladys TISBA <gladys.tisba@ens.uvsq.fr>
2023-03-24 14:08:06 +0100

    ok

--- TD/TD2/cible.py
+++ TD/TD2/cible.py
@@ -6,7 +6,7 @@ canvas = tk.Canvas(root, width = 540, height = 540, bg = "dark turquoise")
 canvas.grid(row=1, column=1)
 
 
-coul = [ "blue", "green","black", "yellow","magenta", "red", "blue", "green","black", "yellow","magenta", "red","blue", "green","black"]
+coul = [ "blue", "green","black", "yellow","magenta", "red", "blue", "green","black", "yellow","magenta", "red","blue", "green","black", ]
 def cercle(n,x0,y0,x1,y1,r):
     for i in range (n):
         canvas.create_oval((x0+r, y0+r), (x1-r, y1-r), fill=coul[i]) 
